9374478153e437c4b8beabaa9cc9ae307c4938ea
bartek <bartek.k2906@gmail.com>
2024-04-11 14:23:10 +0200

    Initial Commit

---
+++ projekt2_blackjack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  projekt2_blackjack
+//
+//  Created by Bartłomiej Kuś on 11/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
